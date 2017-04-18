13f879aae344e9d86f2342e161a15a1256e8c24c
Gilad Lev Ari <GiladL@cet.ac.il>
2017-04-18 16:13:50 +0300

    Adding Search route

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
+import {HashRouter, Route} from 'react-router-dom'
+import Landing from './Landing'
+import Search from './Search'
 import '../public/normalize.css'
 import '../public/style.css'
 
 const App = React.createClass({
   render () {
     return (
-      <div className='app'>
-        null
-      </div>
+      <HashRouter>
+        <div className='app'>
+          <Route exact path='/' component={Landing} />
+          <Route path='/search' component={Search} />
+        </div>
+      </HashRouter>
     )
   }
 })
