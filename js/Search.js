4bddbe3395ef377593ed33962b1fca46cb815145
Gilad Lev-Ari <giladl82@gmail.com>
2017-04-18 20:57:13 +0300

    Adding state to search input

--- js/Search.js
+++ js/Search.js
@@ -3,6 +3,14 @@ import preload from '../public/data.json'
 import ShowCard from './ShowCard'
 
 const Search = React.createClass({
+  getInitialState () {
+    return {
+      searchTerm: ''
+    }
+  },
+  handleSearchTermChange (event) {
+    this.setState({ searchTerm: event.target.value })
+  },
   render () {
     return (
       <div className='search'>
@@ -10,14 +18,14 @@ const Search = React.createClass({
           <h1>
             svideo
           </h1>
-          <input type='text' placeholder='Search' />
+          <input type='text' placeholder='Search' value={this.state.searchTerm} onChange={this.handleSearchTermChange} />
         </header>
         <div>
           {preload.shows.map((show) => {
             return (
               <ShowCard {...show} key={show.imdbID} />
             )
-          }) }
+          })}
         </div>
       </div>
     )
