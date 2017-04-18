3604ff1dd862fee9bb0b80ab969ce0bef9d48583
Gilad Lev Ari <GiladL@cet.ac.il>
2017-04-18 16:25:27 +0300

    moving to browser history + link to search

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {HashRouter, Route} from 'react-router-dom'
+import {BrowserRouter, Route} from 'react-router-dom'
 import Landing from './Landing'
 import Search from './Search'
 import '../public/normalize.css'
@@ -9,12 +9,12 @@ import '../public/style.css'
 const App = React.createClass({
   render () {
     return (
-      <HashRouter>
+      <BrowserRouter>
         <div className='app'>
           <Route exact path='/' component={Landing} />
           <Route path='/search' component={Search} />
         </div>
-      </HashRouter>
+      </BrowserRouter>
     )
   }
 })
