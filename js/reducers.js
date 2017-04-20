6da3076b3c4a816a68cd1e031984e2e92a04f315
Gilad Lev Ari <GiladL@cet.ac.il>
2017-04-20 10:47:16 +0300

    Adding async actions

--- js/reducers.js
+++ js/reducers.js
@@ -1,12 +1,26 @@
-import { SET_SEARCH_TERM } from './actions'
+import { SET_SEARCH_TERM, ADD_OMDB_DATA } from './actions'
 
 const DEFAULT_STATE = {
-  searchTerm: ''
+  searchTerm: '',
+  omdbData: {}
 }
 
-const setSearchTerm = (state = DEFAULT_STATE, action) => {
+const setSearchTerm = (state, action) => {
   const newState = {}
   Object.assign(newState, state, { searchTerm: action.searchTerm })
+  return newState
+}
+/**
+ * Adds new OMDB data
+ * @param {Object} state The current state
+ * @param {Object} action Dispatched action {type:ADD_OMDB_DATA, omdbData: {...}, imdbID: 'tt12345' }
+ * @returns {Object} The new state
+ */
+const addomdbData = (state, action) => {
+  const newOmdbData = {}
+  Object.assign(newOmdbData, state.omdbData, { [action.imdbID]: action.omdbData })
+  const newState = {}
+  Object.assign(newState, state, { omdbData: newOmdbData })
   return newState
 }
 
@@ -14,6 +28,8 @@ const rootReducer = (state = DEFAULT_STATE, action) => {
   switch (action.type) {
     case SET_SEARCH_TERM:
       return setSearchTerm(state, action)
+    case ADD_OMDB_DATA:
+      return addomdbData(state, action)
     default:
       return state
   }
