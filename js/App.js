1be6a5df8b9a92bef6fca5427a32163623fd8816
Gilad Lev Ari <GiladL@cet.ac.il>
2017-04-20 16:12:06 +0300

    server side rendering

---
+++ js/App.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Route } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import store from './store'
+import Landing from './Landing'
+import Search from './Search'
+import Details from './Details'
+import preload from '../public/data.json'
+// import '../public/normalize.css'
+// import '../public/style.css'
+
+const App = () => {
+  return (
+    <Provider store={store}>
+      <div className='app'>
+        <Route exact path='/' component={Landing} />
+        <Route path='/search'
+          component={(props) => <Search shows={preload.shows} {...props} />} />
+        <Route path='/details/:id'
+          component={(props) => {
+            const shows = preload.shows.filter((show) => {
+              return props.match.params.id === show.imdbID
+            })
+
+            return <Details show={shows[ 0 ]} {...props} />
+          }} />
+      </div>
+    </Provider>
+  )
+}
+
+export default App
--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,37 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter, Route } from 'react-router-dom'
-import { Provider } from 'react-redux'
-import store from './store'
-import Landing from './Landing'
-import Search from './Search'
-import Details from './Details'
-import preload from '../public/data.json'
-import '../public/normalize.css'
-import '../public/style.css'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App'
 
-const App = React.createClass({
-  render () {
-    return (
-      <BrowserRouter>
-        <Provider store={store}>
-          <div className='app'>
-            <Route exact path='/' component={Landing} />
-            <Route path='/search'
-              component={(props) => <Search shows={preload.shows} {...props} />} />
-            <Route path='/details/:id'
-              component={(props) => {
-                const shows = preload.shows.filter((show) => {
-                  return props.match.params.id === show.imdbID
-                })
-
-                return <Details show={shows[0]} {...props} />
-              }} />
-          </div>
-        </Provider>
-      </BrowserRouter>
-    )
-  }
-})
-
-ReactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('app'))
